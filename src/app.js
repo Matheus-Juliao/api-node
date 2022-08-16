32dddfb8d2de3976ac231fd21418ef3d36301f19
Matheus.Juliao <matheus.juliao@stoneridge.com>
2022-08-16 12:13:48 -0300

    finished api-node

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
-import { createTable } from './controler/Pessoa.js';
 import  express  from 'express';
+import router from './routes.js';
+import { createTable } from './controler/Pessoa.js';
+import fs from 'fs';
+import https from 'https';
+import cors from 'cors';
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
+app.use(router);
 
 createTable();
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
-});
-
-app.post('/pessoa', (req, res) => {
-    //console.log(res);
-    console.log(req.body);
-    res.json({
-        "statusCode": 200
-    })
-});
-
 app.listen(3000, ()=>console.log("API rodando"));
+
+https.createServer({
+    cert: fs.readFileSync('src/SSL/code.crt'),
+    key: fs.readFileSync('src/SSL/code.key')
+}, app).listen(3001, ()=> console.log('Rodando em https'));
---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { createTable, insertPessoa, updatePessoa, selectPessoas, selectPessoa, deletePessoa } from './controler/Pessoa.js';
+
+const router = Router();
+
+router.get('/', (req, res)=> {
+    res.json({
+        "statusCode": 200,
+        "msg": "Api rodando!"
+    })
+});
+
+
+router.get('/pessoas', selectPessoas);
+router.get('/pessoa', selectPessoa);
+router.post('/pessoa', insertPessoa);
+router.put('/pessoa', updatePessoa);
+router.delete('/pessoa', deletePessoa);
+
+export default router;
